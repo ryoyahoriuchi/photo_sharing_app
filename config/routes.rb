630b7a0e939a62c526140890259a905d34601ed5
ryoya horiuchi <appleorange0720@gmail.com>
2022-02-14 23:25:35 +0900

    ログイン機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+  resources :users, only: %i( new create show edit )
+  resources :sessions, only: %i( new create destroy )
   resources :photos do
     collection do
       post :confirm
