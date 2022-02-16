9b86ef81a16869419d95719c25d8d4b5e46491cc
ryoya horiuchi <appleorange0720@gmail.com>
2022-02-16 21:55:14 +0900

    メールアドレス記載方法を修正

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV[MAILADRESS]#'appleorange0720@gmail.com'
+  default from: 'appleorange0720@gmail.com'#ENV[MAILADRESS]
   layout 'mailer'
 end
