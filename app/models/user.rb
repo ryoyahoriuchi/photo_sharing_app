253de4dc56f00a8c764cb777299e66ccc168122f
ryoya horiuchi <appleorange0720@gmail.com>
2022-02-17 16:12:13 +0900

    mailadressにバリデーション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   mount_uploader :image, ImageUploader
   validates :name, presence: true, length: { maximum: 30 }
   validates :email, presence: true, length: { maximum: 255 }, format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i }
+  validates :email, uniqueness: true
 
   before_validation {email.downcase!}
 
